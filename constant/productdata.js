3f390100eaa0630fe287c423c1cc462005c40d6d
Atharv Atre <91801614+atharvatre@users.noreply.github.com>
2022-07-15 14:08:15 +0530

    Add files via upload

---
+++ constant/productdata.js
@@ -0,0 +1,108 @@
+ const products = [
+    { 
+        id: 'product1',
+        url: 'https://m.media-amazon.com/images/I/71xb2xkN5qL._SL1500_.jpg'
+, 
+
+detailUrl: 'https://m.media-amazon.com/images/I/71xb2xkN5qL._SL1500_.jpg', 
+        title: {
+            shortTitle: 'SmartPhones',
+            longTitle: "Apple iPhone 13 Pro Max (128GB) - Sierra Blue",
+        }, 
+        price: {
+            mrp: 114741,
+            cost: 97530,
+            discount: '15%'
+        },
+        description: "With a new display, larger battery and better camera hardware and software, the iPhone 13 is the new benchmark for flagship phones",
+        discount: "Grab Now",
+        tagline: 'Best Seller', 
+    },
+    { 
+        id: 'product2',
+        url: 'https://m.media-amazon.com/images/I/61u9rpZnleS._SL1500_.jpg', 
+        detailUrl: 'https://m.media-amazon.com/images/I/61u9rpZnleS._SL1500_.jpg',
+        title: {
+            shortTitle:"Smart Watches",
+            longTitle:'Garmin Venu Square - Shadow Grey/Slate Smartwatch',
+        },
+        price: {
+            mrp: 17990,
+            cost: 14390,
+            discount: '20%'
+        },
+        description:"THE VENU SQ IS READY FOR ANY KIND OF WORKOUT, INDOORS OR OUTDOORS.",
+        discount: 'Upto 40% off', 
+        tagline: 'Smart watches and more' 
+    },
+    { 
+        id: 'product3',
+        url: 'https://m.media-amazon.com/images/I/817doxCSq7S._SL1500_.jpg', 
+        detailUrl: 'https://m.media-amazon.com/images/I/817doxCSq7S._SL1500_.jpg', 
+        title: {
+            shortTitle:"Ipads/Tablets",
+                           longTitle:'2021 Apple iPad Pro with Apple M1 chip (11-inch/27.96 cm, Wi-Fi, 256GB) - Silver (3rd Generation)',
+        }, 
+        price: {
+            mrp: 80900,
+            cost: 66338,
+            discount: '18%'
+        },
+        description:"The 8-core CPU of M1 delivers up to 50% faster performance. And M1 has an 8-core GPU in a class of its own, delivering up to 40% faster graphics performance to iPad Pro.",
+        discount: 'Upto 30% Off', 
+        tagline: 'Deal of the Day' 
+    },
+    { 
+        id: 'product4',
+        url: "https://m.media-amazon.com/images/I/71g9uJNayaL._SL1024_.jpg", 
+        detailUrl:"https://m.media-amazon.com/images/I/71g9uJNayaL._SL1024_.jpg" ,
+        title: {
+            shortTitle:"Gaming Laptops",
+            longTitle:'Acer Nitro 5 Gaming Laptop/ 11th Gen Intel Core i5-11400H Processor 6 core/ 15.6"(39.6cms) FHD 144 Hz Display (8GB/512GB SSD/GTX 1650 Graphics/Windows 11 Home/RGB), AN515-57 + Xbox Game Pass Ultimate',
+        }, 
+        price: {
+            mrp: 62990,
+            cost: 54990,
+            discount: '12%'
+        },
+        description:"Acer Nitro 5 (2022) is a Windows 10 Home laptop with a 15.00-inch display that has a resolution of 1920x1080 pixels. It is powered by a Core i5 processor and it comes with 8GB of RAM.",
+        discount: 'Grab Now', 
+        tagline: 'Best Seller' 
+    },
+    { 
+        id: 'product5',
+        url: "https://m.media-amazon.com/images/I/71LAxCIdzRL._SL1500_.jpg",
+        detailUrl: "https://m.media-amazon.com/images/I/71LAxCIdzRL._SL1500_.jpg", 
+        title: {
+            shortTitle:"Home",
+            longTitle:'Plantex Premium Steel Folding Step Ladder for Home - Wide Anti Skid Steps (Blue & Black) (6 Step)',
+        }, 
+        price: {
+            mrp: 3989,
+            cost: 2699,
+            discount: '32%'
+        },
+        description:"Plantex steel folding ladder",
+        discount:"Grab Now",
+        tagline:"BestSeller", 
+    },
+    { 
+        id: 'product6',
+        url: "https://m.media-amazon.com/images/I/81ABJMRLcxL._SL1500_.jpg",
+        detailUrl: "https://m.media-amazon.com/images/I/81ABJMRLcxL._SL1500_.jpg", 
+        title: {
+            shortTitle:"TV & Entertainment",
+            longTitle:'Sony Bravia 195 cm (77 inches) XR series 4K Ultra HD Smart OLED Google TV XR-77A80J (Black) (2021 Model) | with Alexa Compatibility',
+        }, 
+        price: {
+            mrp: 522490,
+            cost: 313494,
+            discount: '40%'
+        },
+        description:"",
+        discount:"Grab Now",
+        tagline:"BestSeller", 
+    },
+]
+
+module.exports = products;
